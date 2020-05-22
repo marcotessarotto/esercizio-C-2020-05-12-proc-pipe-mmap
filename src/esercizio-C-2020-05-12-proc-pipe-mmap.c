cb0eef1dfe7adf635a30d48c764498628739a890
AndreaGonzato <gonzatoandrea98@gmail.com>
2020-05-22 19:19:55 +0200

    completed

--- src/esercizio-C-2020-05-12-proc-pipe-mmap.c
+++ src/esercizio-C-2020-05-12-proc-pipe-mmap.c
@@ -14,11 +14,19 @@
 #include <openssl/evp.h>
 #include <string.h>
 
-void child_proces(void);
-void father_proces(void);
+#include <errno.h>
+
+
+void child_process(void);
+void father_process(void);
 
 #define HANDLE_ERROR(msg) { fprintf(stderr, "%s\n", msg); exit(EXIT_FAILURE); }
 #define HANDLE_ERROR2(msg, mdctx) { fprintf(stderr, "%s\n", msg); EVP_MD_CTX_destroy(mdctx); exit(EXIT_FAILURE); }
+
+#define CHECK_ERR(a,msg) {if ((a) == -1) { perror((msg)); exit(EXIT_FAILURE); } }
+#define CHECK_ALLOC(a,msg) {if ((a) == NULL) { perror((msg)); exit(EXIT_FAILURE); } }
+#define CHECK_ERR_MMAP(a,msg) {if ((a) == MAP_FAILED) { perror((msg)); exit(EXIT_FAILURE); } }
+
 
 #define BUFFER_SIZE 1024
 #define MAP_SIZE 64
@@ -27,40 +35,36 @@ int pfd[2];
 // pfd[0]: file descriptor for READING from PIPE
 // pfd[1]: file descriptor for WRITING from PIPE
 
-char * addr;
+char * map;
 char * file = "/home/andrea/Scrivania/test";
 
 int main(int argc, char *argv[])
 {
+    if(argc >= 2){
+    	file = argv[1];
+    }else{
+    	perror("give a file name as argument");
+    	exit(1);
+    }
+
 	// create pipe
 	if (pipe(pfd) == -1) {
-		perror("problema con pipe");
+		perror("pipe()\n");
 		exit(EXIT_FAILURE);
 	}
 
-
 	// create memory map
-	addr = mmap(NULL, // NULL: è il kernel a scegliere l'indirizzo
+	map = mmap(NULL, // NULL: è il kernel a scegliere l'indirizzo
 			MAP_SIZE, // dimensione della memory map
 			PROT_READ | PROT_WRITE, // memory map leggibile e scrivibile
 			MAP_SHARED | MAP_ANONYMOUS, // memory map condivisibile con altri processi
 			-1,
 			0); // offset nel file
-	if (addr == MAP_FAILED) {
+	if (map == MAP_FAILED) {
 		printf("ciao3");
 		perror("mmap()");
 		exit(EXIT_FAILURE);
 	}
-
-
-
-    if(argc >= 2){
-    	file = argv[1];
-    	printf("%s\n", file);
-    }else{
-    	printf("in this program you can spec a file to calculate sha_512, just pass a file name as argument\n");
-    }
-
 
 	pid_t child = fork();
 
@@ -69,11 +73,11 @@ int main(int argc, char *argv[])
 			perror("fork()\n");
 			exit(1);
 		case 0:
-			child_proces();
+			child_process();
 			exit(0);
 		default :
 		{
-			father_proces();
+			father_process();
 
 			// wait the child
 			if(wait(NULL) == -1){
@@ -85,27 +89,25 @@ int main(int argc, char *argv[])
 
 	printf("SHA3_512 del file %s è il seguente:\n", file);
 	for (int i = 0; i < MAP_SIZE; i++) {
-		printf("%02x", addr[i] & 0xFF);
+		printf("%02x", map[i] & 0xFF);
 	}
+	printf("\n");
 	printf("\n");
 
     exit(0);
 }
 
-void child_proces(void){
+void child_process(void){
 
-	close(pfd[1]); // close write part of the pipe
+	close(pfd[1]); // close write side of the pipe
 
     ssize_t bytesRead;
-    char * buffer;
 
     // from sha512
     EVP_MD_CTX * mdctx;
-	int val;
 	unsigned char * digest;
 	unsigned int digest_len;
 	EVP_MD * algo = NULL;
-
 	algo = EVP_sha3_512();
 
 	if ((mdctx = EVP_MD_CTX_create()) == NULL) {
@@ -117,23 +119,17 @@ void child_proces(void){
 		HANDLE_ERROR2("EVP_DigestInit_ex() error", mdctx)
 	}
 
-	while ((bytesRead = read(pfd[0], buffer, BUFFER_SIZE)) > 0) {
-		// TEST
-		for(int i=0 ; i< bytesRead ; i++){
-			printf("%c", buffer[i]);
-		}
+    char * buffer2 = malloc(BUFFER_SIZE * sizeof(char));
+    if(buffer2 == NULL){
+    	perror("malloc()\n");
+    	exit(1);
+    }
 
+	while ((bytesRead = read(pfd[0], buffer2, BUFFER_SIZE)) > 0) {
 		// provide data to digest engine
-		if (EVP_DigestUpdate(mdctx, buffer, bytesRead) != 1) { // returns 1 if successful
+		if (EVP_DigestUpdate(mdctx, buffer2, bytesRead) != 1) { // returns 1 if successful
 			HANDLE_ERROR2("EVP_DigestUpdate() error", mdctx)
 		}
-
-		// buffer for next read
-		buffer = malloc(BUFFER_SIZE * sizeof(char));
-	    if(buffer == NULL){
-	    	perror("malloc()\n");
-	    	exit(1);
-	    }
 	}
 
 	close(pfd[0]);
@@ -150,20 +146,15 @@ void child_proces(void){
 		HANDLE_ERROR2("EVP_DigestFinal_ex() error", mdctx)
 	}
 
-	memcpy(addr, digest, digest_len);
+	memcpy(map, digest, digest_len);
 
 	OPENSSL_free(digest);
 	EVP_MD_CTX_destroy(mdctx);
-
-	printf("SHA3_512 del file %s è il seguente:\n", file);
-	for (int i = 0; i < MAP_SIZE; i++) {
-		printf("%02x", addr[i] & 0xFF);
-	}
-	printf("\n");
+	free(buffer2);
 }
 
 
-void father_proces(void){
+void father_process(void){
 	close(pfd[0]); // close the reading pipe
 
     char * buffer = malloc(BUFFER_SIZE * sizeof(char));
@@ -177,14 +168,8 @@ void father_proces(void){
 
 	while ((bytesRead = read(fd, buffer, BUFFER_SIZE)) > 0) {
 		write(pfd[1], buffer, bytesRead);
-
-		// buffer for next read
-		buffer = malloc(BUFFER_SIZE * sizeof(char));
-	    if(buffer == NULL){
-	    	perror("malloc()\n");
-	    	exit(1);
-	    }
 	}
 	close(pfd[1]);
+	close(fd);
 	free(buffer);
 }
